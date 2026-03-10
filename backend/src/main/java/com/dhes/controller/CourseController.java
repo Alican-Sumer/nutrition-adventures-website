7c6866fba17bc92b587df66ce74d19dc101a8a76
MatthewShooman <mtshooma@ncsu.edu>
2026-03-10 16:52:20 -0400

    start on controller classes

--- backend/src/main/java/com/dhes/controller/CourseController.java
+++ backend/src/main/java/com/dhes/controller/CourseController.java
@@ -2,8 +2,10 @@ package com.dhes.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,18 +22,30 @@ public class CourseController {
 
     private CourseService courseService;
     private UserService userService;
-    
+
     public CourseController(CourseService courseService, UserService userService) {
         this.courseService = courseService;
         this.userService = userService;
     }
 
+    @PostMapping
     private ResponseEntity<CourseDto> createCourse(CourseDto courseDto) {
         this.courseService.createCourse(courseDto);
-    
+        return ResponseEntity.ok(courseDto);
     }
-+ getCourseById(Long): Course
-+ getAllCourses(): List<Course>
+
+    @GetMapping
+    public ResponseEntity<CourseDto> getCourseById(Long courseId) {
+        CourseDto courseDto = this.courseService.getCourseById(courseId);
+        if (courseDto != null) return ResponseEntity.ok(courseDto);
+        else return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+
+    @GetMapping
+    public ResponseEntity<List<Course>> getAllCourses() {
+        
+    }
 + updateCourse(Long): CourseDto
 + deleteCourse(Long): void
 + addUser(Long, Long): CourseDto
